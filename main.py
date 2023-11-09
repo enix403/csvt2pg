42ccab66af2a53892c631e6d7ab7944896ef0d00
enix403 <fog.code000@gmail.com>
2023-11-09 13:47:17 +0500

    Argument parsing

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-import click
 import csv
+import argparse
 from pathlib import Path
 
+import click
 from sqlalchemy import create_engine, Engine, text, DDL
 from sqlalchemy.engine import URL
 
 from configparser import ConfigParser
 
-config = ConfigParser()
-with open("config.txt") as stream:
-    # Append a fake section
-    config.read_string("[top]\n" + stream.read())
+class GlobalConfig:
+    C_CONFIGDIR: Path
+    C_DB_NAME: str
+    C_DB_USER: str
+    C_DB_PASSWORD: str
+    C_DB_HOST: str
+    C_DB_PORT: str
+    C_TABLE_NAME: str
+    C_CSV_DIRECTORY: str
 
-config = config['top']
+g = GlobalConfig()
 
-C_DB_NAME = config.get('db_name', None)
-C_DB_USER = config.get('db_user', None)
-C_DB_PASSWORD = config.get('db_password', None)
-C_DB_HOST = config.get('db_host', None)
-C_DB_PORT = config.get('db_port', None)
-C_TABLE_NAME = config.get('table_name', None)
-C_CSV_DIRECTORY = config.get('csv_directory', None)
+def init_config(config_file_loc: str):
+    config = ConfigParser()
+    with open(config_file_loc) as stream:
+        # Append a fake section
+        config.read_string("[top]\n" + stream.read())
+
+    config = config['top']
+
+    configdir = Path(config_file_loc).parent.resolve()
+
+    g.C_CONFIGDIR = configdir
+    g.C_DB_NAME = config.get('db_name', None)
+    g.C_DB_USER = config.get('db_user', None)
+    g.C_DB_PASSWORD = config.get('db_password', None)
+    g.C_DB_HOST = config.get('db_host', None)
+    g.C_DB_PORT = config.get('db_port', None)
+    g.C_TABLE_NAME = config.get('table_name', None)
+    g.C_CSV_DIRECTORY = config.get('csv_directory', None)
 
 SCHEMA_NAME = "public"
 
@@ -34,11 +51,11 @@ def parse_port(port: str):
 def create_url(use_database: bool = True):
     return URL.create(
         drivername="postgresql",
-        username=C_DB_USER,
-        password=C_DB_PASSWORD,
-        host=C_DB_HOST,
-        port=parse_port(C_DB_PORT),
-        database=C_DB_NAME if use_database else None
+        username=g.C_DB_USER,
+        password=g.C_DB_PASSWORD,
+        host=g.C_DB_HOST,
+        port=parse_port(g.C_DB_PORT),
+        database=g.C_DB_NAME if use_database else None
     )
 
 def ensure_db():
@@ -51,13 +68,13 @@ def ensure_db():
         text(
             f"SELECT 1 FROM pg_database WHERE datname = :dbname"
         ),
-        { 'dbname': C_DB_NAME }
+        { 'dbname': g.C_DB_NAME }
     )
 
     if result.rowcount == 0:
         conn.execute(text("COMMIT"))
         conn.execute(
-            text(f"CREATE DATABASE {C_DB_NAME}"),
+            text(f"CREATE DATABASE {g.C_DB_NAME}"),
         )
         print("Database creation: Done")
     else:
@@ -65,13 +82,13 @@ def ensure_db():
 
     conn.close()
 
-engine = create_engine(create_url())
+engine: Engine
 
 def table_exists():
     with engine.connect() as conn:
         result = conn.execute(
             text("SELECT 1 FROM pg_catalog.pg_tables WHERE tablename = :tablename"),
-            {'tablename': C_TABLE_NAME}
+            {'tablename': g.C_TABLE_NAME}
         )
 
         return result.rowcount > 0
@@ -81,7 +98,7 @@ def delete_table():
         print("Skip")
         return
 
-    stmt = "DROP TABLE {}".format(C_TABLE_NAME)
+    stmt = "DROP TABLE {}".format(g.C_TABLE_NAME)
 
     with engine.begin() as conn:
         # print(stmt)
@@ -89,7 +106,7 @@ def delete_table():
 
 def create_table(csv_columns):
     columns = []
-    with open('columns.txt') as f:
+    with open(g.C_CONFIGDIR / 'columns.txt') as f:
         for line in f:
             parts = line.strip().split(" ")
             name = parts[0]
@@ -107,8 +124,7 @@ def create_table(csv_columns):
 
     syn = ",\n".join([f"\"{col[0]}\" {col[1]}" for col in columns])
 
-    stmt = "CREATE TABLE {} ({});".format(C_TABLE_NAME, syn)
-    # print(stmt)
+    stmt = "CREATE TABLE {} ({});".format(g.C_TABLE_NAME, syn)
 
     with engine.begin() as conn:
         conn.execute(DDL(stmt))
@@ -117,7 +133,7 @@ def create_table(csv_columns):
 
 
 def init_import(needs_create_table: bool):
-    folder = Path('./csvs')
+    folder = Path(g.C_CSV_DIRECTORY)
     files = sorted(folder.glob('*.csv'))
 
     if len(files) == 0:
@@ -136,7 +152,7 @@ def init_import(needs_create_table: bool):
         col_count = len(active_chunk[0])
 
         placeholder_values = ",".join([f":v{i}" for i in range(col_count)])
-        stmt = "INSERT INTO {} VALUES ({})".format(C_TABLE_NAME, placeholder_values)
+        stmt = "INSERT INTO {} VALUES ({})".format(g.C_TABLE_NAME, placeholder_values)
         values = [
             {
                 f"v{i}": val
@@ -170,11 +186,21 @@ def init_import(needs_create_table: bool):
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Import data from CSV files to PostgreSQL')
+
+    parser.add_argument('config_file', action='store', type=str, help="location of config.txt file")
+    parser.add_argument('--debug', action='store_true', help="Enable debug mode")
+
+    args = parser.parse_args()
+
+    init_config(args.config_file)
+    engine = create_engine(create_url())
+
     delete_all = click.confirm("Delete all data in table?", default=False)
     import_all = click.confirm("Import all data?", default=False)
 
     command = (delete_all, import_all)
-    # command = (True, True)
 
     if command == (True, True):
         # Completely delete table
@@ -187,3 +213,4 @@ if __name__ == "__main__":
         init_import(needs_create_table=create_tbl)
     elif command == (False, False):
         print("Quitting")
+
